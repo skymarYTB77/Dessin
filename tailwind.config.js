70f7ce75fff80149cb91c100a84271101e8c5b6a
skymarYTB77 <kristopher349@gmail.com>
2025-03-30 20:57:43 +0200

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -14,4 +15,4 @@ module.exports = {
     },
   },
   plugins: [],
-} 
+}
